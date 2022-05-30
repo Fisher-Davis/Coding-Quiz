34c9a24d76e83ff74f00efeb86c1092af256e8e0
Fisher Davis <fisherd02@yahoo.com>
2022-05-29 22:04:16 -0400

    pushing coding quiz to github

---
+++ js/questions.js
@@ -0,0 +1,33 @@
+var questions = [
+  {
+    question:
+      "What css property changes the color of the background of your content div?",
+    choices: ["list-style", "margin", "background color", "display"],
+    answer: "background color",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
+    answer: "all of the above",
+  },
+  {
+    question: "How do you log an error message to the console of a page?",
+    choices: ["string", "console.log()", "new Date()", "Syntax"],
+    answer: "console.log()",
+  },
+  {
+    question: "What do you use when you want to loop through an array?",
+    choices: ["while-loop", "for-loop", "array-loop", "web API"],
+    answer: "for-loop",
+  },
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  }
+];
